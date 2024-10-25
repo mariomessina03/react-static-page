10c5d0f0d7c69eefd87410c27b75058a1afd0bfd
mariomessina03 <171628699+mariomessina03@users.noreply.github.com>
2024-10-25 06:41:13 +0200

    Implemented dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "../assets/react-logo.png";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <div className="navigation">
-      <img src={logo}></img>
-      <h1>ReactFacts</h1>
-    </div>
+    <nav className={props.darkMode ? "dark" : ""}>
+      <img className="nav--logo_icon" src={logo} />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
   );
 }
